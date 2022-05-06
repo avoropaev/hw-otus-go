427032d2541c676eb4e0975c0046e35474b6d3fa
VoropaevAE <aleksejj.voropaev@vseinstrumenti.ru>
2022-05-06 09:12:29 +0600

    hw11_telnet_client: HW11 is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -13,9 +16,71 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	conn    net.Conn
+	in      io.ReadCloser
+	out     io.Writer
+}
+
+func (c *telnetClient) Close() error {
+	return c.conn.Close()
+}
+
+func (c *telnetClient) Send() error {
+	if c.conn == nil {
+		return errors.New("need to connect first")
+	}
+
+	_, err := io.Copy(c.conn, c.in)
+	if err != nil {
+		return err
+	}
+
+	if _, err = os.Stderr.Write([]byte("...EOF\n")); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *telnetClient) Receive() error {
+	if c.conn == nil {
+		return errors.New("need to connect first")
+	}
+
+	_, err := io.Copy(c.out, c.conn)
+	if err != nil {
+		return err
+	}
+
+	if _, err = os.Stderr.Write([]byte("...Connection was closed by peer\n")); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *telnetClient) Connect() error {
+	if c.conn != nil {
+		return nil
+	}
+
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+
+	c.conn = conn
+
+	return nil
+}
